b467e4c96054c62bb04ab428ab070ad0d0b6312a
Akram Hossain <dhaliakram6@gmail.com>
2025-11-07 21:18:10 +0600

    pharmacy related api create

---
+++ backend/pharmacy/src/main/java/com/hms/pharmacy/config/CorsConfig.java
@@ -0,0 +1,41 @@
+package com.hms.pharmacy.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    // @Bean
+    // public CorsConfigurationSource corsConfigurationSource() {
+    // CorsConfiguration config = new CorsConfiguration();
+    // config.setAllowedOrigins(List.of("http://localhost:5173"));
+    // config.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+    // config.setAllowCredentials(true);
+    // config.addAllowedHeader("*");
+    // UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    // source.registerCorsConfiguration("/**", config);
+    // return source;
+// }
+
+}
